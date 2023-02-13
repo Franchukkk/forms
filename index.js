59656d39b7a140e9139f4626bf415235ac75f8cc
Oleksandr Franchukkk <sha471251@gmail.com>
2023-02-13 19:24:42 +0200

    exit-back

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 hider.onclick = function (){
-    text.classList.toggle("none")
+    text.style.display = "none"
+    gostart.style.display = "block"
+}
+
+gostart.onclick = function (){
+    text.style.display = "block"
+    gostart.style.display = "none"
 }
 
